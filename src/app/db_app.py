5d5a4c1d7bd45aad1f631a0ad27e746316a565cd
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-08-13 19:14:49 -0300

    feat: Creating db app

---
+++ src/app/db_app.py
@@ -0,0 +1,15 @@
+"""
+Module responsible for creating application instance for database operations.
+"""
+
+import os
+
+from dotenv import load_dotenv
+
+from src.handlers.sql_alchemy_handler import SQLAlchemyHandler
+
+# Load environment variables from a .env file
+load_dotenv()
+
+DB_URL = os.getenv("DB_URL")
+DB_APP = SQLAlchemyHandler(database_url=DB_URL)
