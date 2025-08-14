be75b6b85601acb1cbe5150662a921b2a7a66422
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-08-14 17:19:34 -0300

    feat: Implementing fast api application in main

--- main.py
+++ main.py
@@ -6,13 +6,25 @@ This script initializes the application and starts the main event loop.
 # pylint: disable=W0611
 
 from src.app.db_app import DB_APP
-from src.app.fast_api_app import fast_api_instance
+from src.app.fast_api_app import FAST_API_APP
+from src.app.jwt_app import JWT_APP
 from src.tables.products import Products
 from src.tables.users import Users
+from src.routes.users.users import UserRoute
 
 if __name__ == "__main__":
     # Create database application
     DB_APP.create_all_metadata()
 
+    # Register routes
+    routes_to_register = [UserRoute]
+
+    for route in routes_to_register:
+        route_instance = route(
+            fast_api_instance=FAST_API_APP,
+            jwt_instance=JWT_APP,
+        )
+        route_instance.build_routes()
+
     # Run the FastAPI application
-    fast_api_instance.run_app()
+    FAST_API_APP.run_app()
